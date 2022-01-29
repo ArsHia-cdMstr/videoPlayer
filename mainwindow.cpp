a5023ed13f830073d80c8aac7168b57861a37807
arshia.mkh <arshia.mokhlesi@gmail.com>
2022-01-29 10:37:13 +0330

    movie will play and play , stop , pause and open button works

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,10 +6,47 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    player = new QMediaPlayer(this);
+    vw = new QVideoWidget(this);
+//    vw->setGeometry(10,10,600,200);
+
+    player->setVideoOutput(vw);
+    this->setCentralWidget(vw);
+
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_actionstop_triggered()
+{
+    player->stop();
+}
+
+
+void MainWindow::on_actionpause_triggered()
+{
+    player->pause();
+}
+
+
+void MainWindow::on_actionplay_triggered()
+{
+    player->play();
+}
+
+
+void MainWindow::on_actionopen_triggered()
+{
+    QString fileName = QFileDialog::getOpenFileName(this,"open a file", "" , "video file(*.*)");
+    on_actionstop_triggered();
+
+    player->setMedia(QUrl::fromLocalFile(fileName));
+    on_actionplay_triggered();
+
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QMediaPlayer>
+#include <QVideoWidget>
+#include <QFileDialog>
+#include <QSlider>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +19,20 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+
+    void on_actionstop_triggered();
+
+    void on_actionpause_triggered();
+
+    void on_actionplay_triggered();
+
+    void on_actionopen_triggered();
+
 private:
     Ui::MainWindow *ui;
+    QMediaPlayer* player;
+    QVideoWidget* vw;
+
 };
 #endif // MAINWINDOW_H
